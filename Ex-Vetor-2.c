321cb5b57028c7895eda7ac5a3a71569adbc1e57
Daniel Gonçalves <31700067+danielg04@users.noreply.github.com>
2017-10-17 16:15:38 -0200

    Update Ex-Vetor-2.c

--- Ex-Vetor-2.c
+++ Ex-Vetor-2.c
@@ -8,40 +8,44 @@
  - Caso contrário, imprima: Não foi hoje.*/
 
 int main(void){
-
-int numescolhidos[6], y,x,z,w,t,i,q;
-int sorteados[6];
-y=7;
-x=25;
-z=33;
-w=42;
-t=51;
-q=54;
+int i,x;
+int numescolhidos[6];
+int num[6];
+num[0]=7;
+num[1]=25;
+num[2]=33;
+num[3]=42;
+num[4]=51;
+num[5]=58;
 
 printf("Escolha 6 numeros entre 1 a 60\n");
 
 for(i=0; i!=6; i++){
-    printf("\nDigite o numero %d de 6: \n", i+1);
-    scanf("%d", &numescolhidos[i]);
+    printf("\n%d- numero de 6: ", i+1);
+    scanf("%d",&numescolhidos[i]);
+}
+for(i=0; i!=6; i++){
+    if(numescolhidos[i] == num[i])
+        x++;
 }
 
-sorteados[6] = y,x,z,w,t,q;
+printf("\nVoce acertou: %d numeros\n", x);
 
-printf("\nVoce acertou: %d numeros\n", sorteados[6]);
-
-if(sorteados == 6){
+if(x == 6){
     printf("Ganhou Tudo\n");
 }
-else if(sorteados == 5){
+else if(x == 5){
     printf("Ganhou Quina\n");
 }
-else if(sorteados == 4){
+else if(x == 4){
     printf("Ganhou Quadra\n");
 }
-else if(sorteados == 3){
+else if(x == 3){
     printf("Ganhou Quase\n");
 }
 else
     printf("Nao foi hoje\n");
+
+return 0;
 }
 
