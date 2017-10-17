38e754d7daec2510b071e19bb1340b5a7ed280d3
Daniel Gonçalves <31700067+danielg04@users.noreply.github.com>
2017-10-17 14:58:11 -0200

    Update Ex-Vetor-2.c

--- Ex-Vetor-2.c
+++ Ex-Vetor-2.c
@@ -1,27 +1,47 @@
 #include<stdio.h>
-/*Faça um program que verifique se o usuário ganhou na mega sena.
- O  programa deve ter um vetor estático com os valores sorteados, caso o usuário acerte:
- - 6 números, imprima: Ganhou tudo
- - 5 números, imrpima: Ganhou quina
- - 4 números, imrpima: Ganhou quadra
- - 3 números, imprima: Quase
- - Caso contrário, imprima: Não foi hoje.*/
+/*FaÃ§a um program que verifique se o usuÃ¡rio ganhou na mega sena.
+ O  programa deve ter um vetor estÃ¡tico com os valores sorteados, caso o usuÃ¡rio acerte:
+ - 6 nÃºmeros, imprima: Ganhou tudo
+ - 5 nÃºmeros, imrpima: Ganhou quina
+ - 4 nÃºmeros, imrpima: Ganhou quadra
+ - 3 nÃºmeros, imprima: Quase
+ - Caso contrÃ¡rio, imprima: NÃ£o foi hoje.*/
 
 int main(void){
 
-int a[6], y,x,z,w,t,i;
-int num;
+int numescolhidos[6], y,x,z,w,t,i,q;
+int sorteados[6];
 y=7;
 x=25;
 z=33;
 w=42;
 t=51;
+q=54;
 
-printf("Escolha 6 numeros de 1 a 60\n");
+printf("Escolha 6 numeros entre 1 a 60\n");
 
 for(i=0; i!=6; i++){
     printf("\nDigite o numero %d de 6: \n", i+1);
-    scanf("%d", a);
+    scanf("%d", &numescolhidos[i]);
 }
 
+sorteados[6] = y,x,z,w,t,q;
+
+printf("\nVoce acertou: %d numeros\n", sorteados[6]);
+
+if(sorteados == 6){
+    printf("Ganhou Tudo\n");
 }
+else if(sorteados == 5){
+    printf("Ganhou Quina\n");
+}
+else if(sorteados == 4){
+    printf("Ganhou Quadra\n");
+}
+else if(sorteados == 3){
+    printf("Ganhou Quase\n");
+}
+else
+    printf("Ganhou Nao foi hoje\n");
+}
+
