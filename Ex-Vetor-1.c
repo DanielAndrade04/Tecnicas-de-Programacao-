62385227a80ccfc22ae03fb0ef2eba1bb8ab7330
Daniel Gonçalves <31700067+danielg04@users.noreply.github.com>
2017-10-24 21:28:03 -0200

    Update Ex-Vetor-1.c

--- Ex-Vetor-1.c
+++ Ex-Vetor-1.c
@@ -2,36 +2,37 @@
 /*Faça um programa que receba um numero n, que consiste na quantidade de notas que serão digitadas.Ao final, mostre todas as notas e a media das mesmas.
 Caso a media seja maior que 60, imprima também "Aprovado" caso contrario imprima "Reprovado".*/
 
-int main (){
-int i, b, c;
-float d, media;
-d=0;
+int main (void){
+int i, quant, c;
+float soma, media;
+soma=0;
 
 printf("Digite a quantidade de notas: \n");
-scanf("%d", &b);
+scanf("%d", &quant);
 
-float a[b];
+float notas[quant];
 
-for(i=0; i!=b; i++){
+for(i=0; i!=quant; i++){
 
-printf("\nDigite a nota %d de %d\n", i+1, b);
-scanf("%f", &a[i]);
-d = d + a[i];
+printf("\nDigite a nota %d de %d\n", i+1, quant);
+scanf("%f", &notas[i]);
+soma = soma + notas[i];
 }
 
-media = (d/i);
+media = (soma/i);
 
 printf("\nMedia das notas = %.2f\n", media);
 
     if(media >= 60){
         printf("\nAprovado\n");
     }
-    else if (media <60){
+    else{
         printf("\nReprovado\n");
     }
-    printf("\nNotas digitadas:\n");
+    printf("\nNotas digitadas: ");
 
-for(i=0;i!=b; i++){
-    printf("%.f,", a[i]);
+for(i=0;i!=quant; i++){
+    printf("%.f,", notas[i]);
 }
+return 0;
 }
