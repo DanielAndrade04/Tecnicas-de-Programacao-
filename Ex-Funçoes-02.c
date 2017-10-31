dc66298f55af00ed3bba6e03ac55c3a1e9274029
Daniel Gonçalves <31700067+danielg04@users.noreply.github.com>
2017-10-31 16:31:40 -0200

    Update Ex-Funçoes-02.c

--- Ex-Funçoes-02.c
+++ Ex-Funçoes-02.c
@@ -1,12 +1,27 @@
+/*FaÃ§a um programa que calcule a sequencia fibonacci em n;*/
+
 #include<stdio.h>
-/*Faça um programa que calcule a sequencia fibonacci em n;*/
-int main(){
 
+int main(void){
 
+int a, b, auxiliar, i, n;
+a = 0;
+b = 1;
 
-return 0;
+printf("Digite um numero: ");
+
+scanf("%d", &n);
+printf("Serie de Fibonacci: \n");
+printf("%d\n", b);
+
+for(i = 0; i < n; i++){
+    auxiliar = a + b;
+    a = b;
+    b = auxiliar;
+
+    printf("%d\n", auxiliar);
+  }
 }
-
 
 
 
